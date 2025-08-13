fc0a13fcf21f3170931a2f8e63f9ed6c74034aec
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-13 13:35:29 +0530

    Update merge_two_sorted_arrays_functions.c

--- merge_two_sorted_arrays_functions.c
+++ merge_two_sorted_arrays_functions.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
-void merge_sorted_arrays(int arr1[], int n, int arr2[], int m, int arr3[]) {
+void merge_sorted_arrays(int arr1[], int n, int arr2[], int m, int arr3[]) 
+{
     int i = 0, j = 0, k = 0;
 
-    while (i < n && j < m) 
+    while(i < n && j < m)
     {
-        if (arr1[i] < arr2[j]) 
+        if(arr1[i] < arr2[j])
         {
-            arr3[k++] = arr1[i++];
+            arr3[k] = arr1[i];
+            i++;
+            k++;
         }
-            
-        else 
+        else
         {
-            arr3[k++] = arr2[j++];
+            arr3[k] = arr2[j];
+            j++;
+            k++;
         }
     }
 
-    while (i < n) 
+    while(i < n)
     {
-        arr3[k++] = arr1[i++];
+        arr3[k] = arr1[i];
+        i++;
+        k++;
     }
 
-    while (j < m) 
+    while(j < m)
     {
-        arr3[k++] = arr2[j++];
+        arr3[k] = arr2[j];
+        j++;
+        k++;
     }
 }
 
@@ -39,11 +47,10 @@ int main()
 
     merge_sorted_arrays(arr1, n, arr2, m, arr3);
 
-    for (int i = 0; i < n + m; i++) 
+    for(int i = 0; i < n + m; i++)
     {
         printf("%d ", arr3[i]);
     }
 
     return 0;
 }
-
