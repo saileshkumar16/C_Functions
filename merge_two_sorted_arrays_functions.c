8fb6780048de73283eaf24614fdb19087b4cc080
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-13 13:35:07 +0530

    Update merge_two_sorted_arrays_functions.c

--- merge_two_sorted_arrays_functions.c
+++ merge_two_sorted_arrays_functions.c
@@ -3,24 +3,32 @@
 void merge_sorted_arrays(int arr1[], int n, int arr2[], int m, int arr3[]) {
     int i = 0, j = 0, k = 0;
 
-    while (i < n && j < m) {
-        if (arr1[i] < arr2[j]) {
+    while (i < n && j < m) 
+    {
+        if (arr1[i] < arr2[j]) 
+        {
             arr3[k++] = arr1[i++];
-        } else {
+        }
+            
+        else 
+        {
             arr3[k++] = arr2[j++];
         }
     }
 
-    while (i < n) {
+    while (i < n) 
+    {
         arr3[k++] = arr1[i++];
     }
 
-    while (j < m) {
+    while (j < m) 
+    {
         arr3[k++] = arr2[j++];
     }
 }
 
-int main() {
+int main() 
+{
     int arr1[] = {1, 3, 5, 7};
     int n = sizeof(arr1) / sizeof(arr1[0]);
 
@@ -31,9 +39,11 @@ int main() {
 
     merge_sorted_arrays(arr1, n, arr2, m, arr3);
 
-    for (int i = 0; i < n + m; i++) {
+    for (int i = 0; i < n + m; i++) 
+    {
         printf("%d ", arr3[i]);
     }
 
     return 0;
 }
+
